08374524d003932d26d4728a3a531c401936c805
Rumble <rumble@tbamud.com>
2017-03-20 11:58:48 +0100

    Fixed empty liquid container bug (thanks WhiskyTest)

--- src/act.item.c
+++ src/act.item.c
@@ -885,7 +885,7 @@ ACMD(do_drink)
     send_to_char(ch, "Your stomach can't contain anymore!\r\n");
     return;
   }
-  if ((GET_OBJ_VAL(temp, 1) == 0) || (GET_OBJ_VAL(temp, 0) != 1)) {
+  if (GET_OBJ_VAL(temp, 1) < 1) {
     send_to_char(ch, "It is empty.\r\n");
     return;
   }
